4372b28c57ce0e3b27c7d473dc5d4327da637b69
bangadjie <masadjie927@gmail.com>
2026-09-04 09:28:15 +0700

    update main langkah 5

--- Jobsheet2/Main.java
+++ Jobsheet2/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Rectangle original = new Rectangle(6,4);
 
+        System.out.println("Area: " + original.area());
+
         Rectangle copy = original;
         copy.width = 10;
         System.out.println("Via original: " + original.area());
         System.out.println("VIa Copy: " + copy.area());  
-        Rectangle empty = null;  
-        System.out.println(empty.area()); 
+        // Rectangle empty = null;  
+        // System.out.println(empty.area()); 
     }
       
 }
