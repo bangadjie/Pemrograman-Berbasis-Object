39afe00e3ae0e039dabb4c43ddd60246341a73c6
bangadjie <masadjie927@gmail.com>
2026-09-04 09:43:57 +0700

    update main langkah ke 7

--- Jobsheet2/Main.java
+++ Jobsheet2/Main.java
@@ -5,19 +5,25 @@ package Jobsheet2;
  */
 public class Main {
     public static void main(String[] args) {
-        Rectangle original = new Rectangle(6,4);
+        Rectangle[] shapes = new Rectangle[3];
+        shapes[0] = new Rectangle(6,4);
+        shapes[1] = new Rectangle(3,3);
+        shapes[2] = new Rectangle(8, 2);
 
-        System.out.println("Area: " + original.area());
-
-        Rectangle copy = original;
-        copy.width = 10;
-        System.out.println("Via original: " + original.area());
-        System.out.println("VIa Copy: " + copy.area());  
+        for(Rectangle r : shapes) {
+            System.out.println("Area: " + r.area() + ", Perimeter: " + r.perimeter());
+        }
+        
+        Student s = new Student("Nadia", "5001", 3.8);
+        System.out.println(s.describe());
+        // System.out.println("Area: " + original.area());
+        // Rectangle copy = original;
+        // copy.width = 10;
+        // System.out.println("Via original: " + original.area());
+        // System.out.println("VIa Copy: " + copy.area());  
         // Rectangle empty = null;  
         // System.out.println(empty.area()); 
 
-        Student s = new Student("Nadia", "5001", 3.8);
-        System.out.println(s.describe());
     }
       
 }
