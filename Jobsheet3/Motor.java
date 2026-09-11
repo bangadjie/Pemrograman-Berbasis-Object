daa8fd26dec08367fd466216c9da4864115ff51a
bangadjie <masadjie927@gmail.com>
2026-09-11 08:29:54 +0700

    add jobsheet 3 percobaan 1

---
+++ Jobsheet3/Motor.java
@@ -0,0 +1,15 @@
+package Jobsheet3;
+
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakon = false;
+
+    public void printStatus(){
+        if (kontakon == true) {
+            System.out.println("Kontak On");
+        }else{
+            System.out.println("Kontak off");
+        }
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+}
