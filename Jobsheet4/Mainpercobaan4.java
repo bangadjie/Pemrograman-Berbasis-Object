aeb9aca2e48d660d632f27f6c7e7bf2c5c47cb82
bangadjie <masadjie927@gmail.com>
2026-09-18 08:29:56 +0700

    feat : Percobaan 4

---
+++ Jobsheet4/Mainpercobaan4.java
@@ -0,0 +1,10 @@
+package Jobsheet4;
+
+public class Mainpercobaan4 {
+    public static void main(String[] args) {
+        Penumpang p = new Penumpang("12345", "Mr. Krab");
+        Gerbong gerbong = new Gerbong("A", 10);
+        gerbong.setPenumpang(p, 1);
+        System.out.println(gerbong.info());
+    }
+}
